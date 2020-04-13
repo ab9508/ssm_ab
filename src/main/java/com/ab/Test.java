0dd88391e4052707097d4e56c83105a721b79e90
ab <ab146268@163.com>
2020-04-13 23:41:46 +0800

    002

---
+++ src/main/java/com/ab/Test.java
@@ -0,0 +1,12 @@
+package com.ab;
+
+/**
+ * @author ab
+ * @description
+ * @date
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("002");
+    }
+}
